4d70128e6d29ca8a869ac274e228ed7163494450
Evelyn Dutra <154927624+dutraevelyn@users.noreply.github.com>
2024-05-29 21:42:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ document.querySelector('.gmail').addEventListener('click', function() {
         form.classList.add('hidden');
     }
 });
+
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal('.titulo', { duration: 1000})
+sr.reveal('.menu', { duration: 1000})
+sr.reveal('.nome', { duration: 1000})
+sr.reveal('#dutra', { duration: 1000})
+sr.reveal('.saudacao', { duration: 1000})
+sr.reveal('.estudante', { duration: 1000})
+sr.reveal('.redes', { duration: 1000})
+sr.reveal('.container', { duration: 1000})
+sr.reveal('.competencias', { duration: 1000})
+sr.reveal('.top', { duration: 1000})
+sr.reveal('.bottom', { duration: 1000})
+sr.reveal('.estrela img', { duration: 1000})
+sr.reveal('.carousel-inner', { duration: 1000})
+sr.reveal('.contatos', { duration: 1000})
